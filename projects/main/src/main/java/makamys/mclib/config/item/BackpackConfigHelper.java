f40ae986976e0277ec70f225747ac4beed8678a2
makamys <makamys@outlook.com>
2021-09-09 12:57:41 +0200

    Support wildcard in blacklist

--- projects/main/src/main/java/makamys/mclib/config/item/BackpackConfigHelper.java
+++ projects/main/src/main/java/makamys/mclib/config/item/BackpackConfigHelper.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import makamys.mclib.core.MCLib;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -12,20 +13,22 @@ public class BackpackConfigHelper {
     
     private static final String TAPED_STORAGE_DRAWERS = "mclib:taped_storage_drawers";
 
-    public static final String CONFIG_DESCRIPTION_SUFFIX = "\n" + TAPED_STORAGE_DRAWERS + " is a special item name that corresponds to taped Storage Drawers drawers\n";
+    public static final String CONFIG_DESCRIPTION_SUFFIX = "\n" + TAPED_STORAGE_DRAWERS + " is a special item name that corresponds to taped Storage Drawers drawers\nThe '*' character can be used as a wildcard to match one or more characters.";
     
-    public static String[] NON_NESTABLE_BACKPACK_BLACKLIST = {TAPED_STORAGE_DRAWERS, "JABBA:moverDiamond", "JABBA:mover", "ExtraUtilities:bedrockiumIngot", "ExtraUtilities:block_bedrockium"};
+    public static String[] NON_NESTABLE_BACKPACK_BLACKLIST = {TAPED_STORAGE_DRAWERS, "JABBA:mover*", "ExtraUtilities:*bedrockium*"};
     
-    private boolean enableDrawers;
+    private boolean blacklistDrawers;
     private Set<Item> blacklistedItems;
     
     public BackpackConfigHelper(List<String> blacklist) {
         if(blacklist.contains(TAPED_STORAGE_DRAWERS)) {
             blacklist.remove(TAPED_STORAGE_DRAWERS);
-            enableDrawers = true;
+            blacklistDrawers = true;
         }
         
         blacklistedItems = ItemConfigHelper.resolveItemList(blacklist).stream().collect(Collectors.toSet());
+        
+        MCLib.LOGGER.debug("Resolved backpack blacklist to " + blacklistedItems);
     }
     
     public boolean isAllowed(ItemStack is) {
--- projects/main/src/main/java/makamys/mclib/config/item/ItemConfigHelper.java
+++ projects/main/src/main/java/makamys/mclib/config/item/ItemConfigHelper.java
@@ -2,6 +2,8 @@ package makamys.mclib.config.item;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
 import net.minecraft.item.Item;
 
 public class ItemConfigHelper {
@@ -9,13 +11,26 @@ public class ItemConfigHelper {
     public static List<Item> resolveItemList(List<String> list){
         List<Item> items = new ArrayList<>();
         for(String itemStr : list) {
-            Object itemObj = Item.itemRegistry.getObject(itemStr);
-            if(itemObj != null) {
-                items.add((Item)itemObj);
-            }
+            items.addAll(resolveItem(itemStr));
         }
         
         return items;
+    }
+    
+    public static List<Item> resolveItem(String itemStr){
+        itemStr = ensureNamespaced(itemStr);
+        List<Item> results = new ArrayList<>();
+        Pattern pattern = Pattern.compile(itemStr.replace(".", "\\.").replace("*", ".*"));
+        for(Object itemNameObj : Item.itemRegistry.getKeys()) {
+            if(pattern.matcher((String)itemNameObj).matches()) {
+                results.add((Item)Item.itemRegistry.getObject(itemNameObj));
+            }
+        }
+        return results;
+    }
+    
+    public static String ensureNamespaced(String name) {
+        return name.indexOf(':') == -1 ? "minecraft:" + name : name;
     }
     
 }
