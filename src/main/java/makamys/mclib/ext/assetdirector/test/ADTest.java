b1c23ac7765810d76d2d5ffb04dd75e0065e13c0
makamys <makamys@outlook.com>
2023-01-14 14:02:21 +0100

    Add 1.19.3 test
    (It fails)

--- src/main/java/makamys/mclib/ext/assetdirector/test/ADTest.java
+++ src/main/java/makamys/mclib/ext/assetdirector/test/ADTest.java
@@ -34,6 +34,7 @@ public class ADTest {
         config.addSoundEvent("1.17", "music_disc.pigstep", "music"); // Test of duplicate sound event
         config.addObject("1.17", "minecraft/sounds/item/bucket/fill_axolotl1.ogg"); // Test downloading single sound file (check the example sounds.json for how to use)
         config.addJar("1.17");
+        config.addObject("1.19.3", "minecraft/lang/nah.json"); // Test of 1.19.3 asset (asset id is '2')
         
         AssetDirectorAPI.register(config);
     }
@@ -44,8 +45,13 @@ public class ADTest {
             Blocks.gold_block.setBlockTextureName("minecraft_1.17:gold_block");
             
             try {
-                JsonObject lang = new Gson().fromJson(new BufferedReader(new InputStreamReader(Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("minecraft_1.17:lang/ms_my.json")).getInputStream(), Charsets.UTF_8)), JsonObject.class);
-                System.out.println("Trident in Malay is " + lang.get("item.minecraft.trident"));
+                JsonObject lang_1_17 = new Gson().fromJson(new BufferedReader(new InputStreamReader(Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("minecraft_1.17:lang/ms_my.json")).getInputStream(), Charsets.UTF_8)), JsonObject.class);
+                // Item added in 1.13
+                System.out.println("Trident in Malay is " + lang_1_17.get("item.minecraft.trident"));
+                
+                JsonObject lang_1_19_3 = new Gson().fromJson(new BufferedReader(new InputStreamReader(Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("minecraft_1.19.3:lang/nah.json")).getInputStream(), Charsets.UTF_8)), JsonObject.class);
+                // Language added in 1.19.3
+                System.out.println("Trident in Nahuatl is " + lang_1_19_3.get("item.minecraft.trident"));
             } catch(IOException e) {
                 e.printStackTrace();
             }
